199f11b923739fd7eef6c6f962f9bedb62104c32
cfrancobedoya <cfrancobedoya@gmail.com>
2017-02-25 17:13:15 -0500

    EJERCICIO 7
    javac JListaDeArgumentos.java
    java JListaDeArgumentos argumento1 argumento2

---
+++ EJERCICIOS/7/JListaDeArgumentos.java
@@ -0,0 +1,12 @@
+// Lista de argumentos en Java
+
+public class JListaDeArgumentos {
+
+    public static void main(String []args) {
+
+	for (int i = 0; i < args.length; i++) {
+
+	    System.out.println("args[" + i + "] = " + args[i]);
+	}
+    }
+}
