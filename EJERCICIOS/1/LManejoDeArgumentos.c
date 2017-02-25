db6428ea98401d71333e090ca94068092799824d
cfrancobedoya <cfrancobedoya@gmail.com>
2017-02-25 12:46:25 -0500

    EJERCICIO 1
    Se compila y se ejecuta el programa LManejoDeArgumentos.c

---
+++ EJERCICIOS/1/LManejoDeArgumentos.c
@@ -0,0 +1,40 @@
+/* LManejoDeArgumentos.c */
+  // $ ./LManejoDeArgumentos -p nada -c
+  // Activada la impresora: nada
+  // Entorno de consola activado
+
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[]) {
+
+  extern char *optarg;
+  extern int optind, opterr, optopt;
+  int option;
+  /*
+   * Verificando los argumentos
+   */
+
+  while ((option = getopt(argc, argv, "p:gc")) != -1) {
+
+    switch (option) {
+    case 'p':
+      fprintf(stdout, "Activada la impresora: %s\n", optarg);
+      break;
+
+    case 'g':
+      fprintf(stdout, "Entorno gr�fico activo\n");
+      break;
+
+    case 'c':
+      fprintf(stdout, "Entorno de consola activado\n");
+      break;
+
+    default:
+      fprintf(stdout, "No se reconoce la opcion\n");
+      break;
+    }
+  }
+
+  return 0;
+}
